f837a209058ef9a8722cb6b4b9a459bfaee42f1f
YuriiSenenko <yurkosenenko@gmail.com>
2022-09-09 12:09:14 +0300

    Task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,41 @@
+const refs = {
+  bodyEl: document.querySelector('body'),
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
 
+// встановлюю сдухачів подій на кнопки
+refs.startBtn.addEventListener('click', onButtonStart);
+refs.stopBtn.addEventListener('click', onButtonStop);
+
+let timeInterval = null;
+
+// деактивую кнопку старт
+// встановлюю інтервал, викликаю функцію заміни кольору
+function onButtonStart() {
+  refs.startBtn.disabled = true;
+  refs.stopBtn.disabled = false;
+  timeInterval = setInterval(() => {
+    changeCokor();
+  }, 1000);
+}
+
+// функція заміни кольору фона
+function changeCokor() {
+  const colorRandom = getRandomHexColor();
+  refs.bodyEl.style.background = colorRandom;
+}
+
+// деактивую кнопку стоп, активую кнопку старт
+// очищаю інтервал
+function onButtonStop() {
+  refs.startBtn.disabled = false;
+  refs.stopBtn.disabled = true;
+
+  clearInterval(timeInterval);
+}
+
+// функція видачі рандомного кольору
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
